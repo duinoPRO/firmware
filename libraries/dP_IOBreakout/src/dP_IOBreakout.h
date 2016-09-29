e8dca240ea9d97ec209ef263f1a06a82a75cd56b
Kristen Clifton <clifton.kristenm@gmail.com>
2016-09-29 15:56:55 +0800

    added dP_IOBreakout src files

---
+++ libraries/dP_IOBreakout/src/dP_IOBreakout.h
@@ -0,0 +1,20 @@
+/*
+ * dP_IOBreakout.h is part of the duinoPRO firmware.
+ *
+ * duinoPRO is an Arduino™-compatible platform in a flat form factor with surface-mount,
+ * solderable modules. It is designed with commercialization of real products in mind.
+ * Note that we have designed duinoPRO to be compatible with the Arduino™ IDE.  This does
+ * not imply that duinoPRO is certified, tested or endorsed by Arduino™ in any way.
+ *
+ * For more information, contact info@duinopro.cc or visit www.duinopro.cc.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * (see https://github.com/duinoPRO/firmware/blob/master/duinoPRO_BSD_fwlicense.txt).
+ *
+ * Using duinoPRO core and libraries licensed under BSD for the firmware of a commercial
+ * product does not require you to release the source code for the firmware.
+ *
+*/
+
+/** This file is deliberately empty and is a place holder.  The dP_IOBreakout library is just
+		an example sketch, but Arduino libraries must also include source files. */
